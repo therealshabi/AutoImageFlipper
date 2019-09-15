2145a3dcb0da454b16b11855887efd797b7f0098
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-09-15 22:45:51 +0530

    Convert all image slider files to Kotlin

---
+++ imageslider/src/main/java/technolifestyle/com/imageslider/FlipperAdapter.kt
@@ -0,0 +1,49 @@
+package technolifestyle.com.imageslider
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager.widget.PagerAdapter
+import java.util.*
+
+internal class FlipperAdapter : PagerAdapter() {
+    private var flipperViews = ArrayList<FlipperView>()
+
+    fun setFlipperViews(flipperViews: ArrayList<FlipperView>) {
+        this.flipperViews = flipperViews
+    }
+
+    fun addFlipperView(view: FlipperView) {
+        flipperViews.add(view)
+        notifyDataSetChanged()
+    }
+
+    fun removeAllFlipperViews() {
+        flipperViews.clear()
+        notifyDataSetChanged()
+    }
+
+    fun getFlipperView(position: Int): FlipperView? {
+        return if (flipperViews.isEmpty() || position >= flipperViews.size) {
+            null
+        } else flipperViews[position]
+    }
+
+    override fun getCount(): Int {
+        return flipperViews.size
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object`
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val imageFlipperView = flipperViews[position]
+        val view = imageFlipperView.getView()
+        container.addView(view)
+        return view
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View)
+    }
+}
---
+++ imageslider/src/main/java/technolifestyle/com/imageslider/FlipperLayout.kt
@@ -0,0 +1,226 @@
+package technolifestyle.com.imageslider
+
+import android.content.Context
+import android.os.Handler
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.FrameLayout
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import java.util.*
+
+/**
+ * FlipperLayout is a compound layout which consists of a View Pager and a View Pager Indicator
+ */
+class FlipperLayout : FrameLayout, CircularFlipperHandler.CurrentPageListener {
+
+    private var flipperLayout: View? = null
+
+    /**
+     * Used for auto cycling to keep the count of current page
+     */
+    private var currentPage = 0
+
+    private lateinit var circularFlipperHandler: CircularFlipperHandler
+
+    /**
+     * Flipping View Pager
+     */
+    private lateinit var mFlippingPager: ViewPager
+
+    /**
+     * Flipping Pager Adapter
+     */
+    private lateinit var mFlippingPagerAdapter: FlipperAdapter
+
+    /**
+     * Getter for FlippingPagerAdapter
+     */
+    private val flippingPagerAdapter: PagerAdapter?
+        get() = mFlippingPagerAdapter
+
+    /**
+     * Runnable to update the current page
+     */
+    private val update = Runnable {
+        if (currentPage == flippingPagerAdapter!!.count) {
+            currentPage = 0
+        }
+        // true set for smooth transition between pager
+        mFlippingPager.setCurrentItem(currentPage, true)
+        currentPage += 1
+    }
+
+    /**
+     * CircleIndicator implementation which Indicates the View Pager
+     */
+    private var circularPagerIndicator: TabLayout? = null
+
+    /**
+     * Scroll Time in seconds
+     */
+    var scrollTimeInSec = 3
+        /**
+         * Setting up Scrolling Time for a page
+         *
+         * @param time in second is sent
+         */
+        set(time) {
+            field = time
+            startAutoCycle()
+        }
+
+    /**
+     * Handler for handling auto cycle
+     */
+    private val flipperHandler: Handler = Handler()
+
+    /**
+     * Scheduler for auto-flipping
+     */
+    private var flippingTimer: Timer? = null
+
+    /**
+     * This returns the current page position of view pager
+     */
+    val currentPagePosition: Int
+        get() = if (flippingPagerAdapter != null) {
+            mFlippingPager.currentItem.rem(mFlippingPagerAdapter.count)
+        } else {
+            throw NullPointerException("Adapter not set")
+        }
+
+    constructor(context: Context) : super(context) {
+        setLayout(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        setLayout(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        setLayout(context)
+    }
+
+    /**
+     * @param width {int} width of circularPagerIndicator
+     */
+    fun setCircleIndicatorWidth(width: Int) {
+        val params = circularPagerIndicator!!.layoutParams
+        params.width = width
+        circularPagerIndicator?.layoutParams = params
+    }
+
+    /**
+     * @param height {int} height of circularPagerIndicator
+     */
+    fun setCircleIndicatorHeight(height: Int) {
+        val params = circularPagerIndicator!!.layoutParams
+        params.height = height
+        circularPagerIndicator?.layoutParams = params
+    }
+
+    /**
+     * @param width  {int} width of circularPagerIndicator
+     * @param height {int} height of circularPagerIndicator
+     */
+    fun setCircularIndicatorLayoutParams(width: Int, height: Int) {
+        val params = circularPagerIndicator!!.layoutParams
+        params.height = height
+        params.width = width
+        circularPagerIndicator?.layoutParams = params
+    }
+
+    /**
+     * Method to remove circlePagerIndicator from viewFlipper
+     */
+    fun removeCircleIndicator() {
+        circularPagerIndicator?.removeAllTabs()
+    }
+
+    /**
+     * Method to show circlePagerIndicator for viewFlipper
+     */
+    fun showCircleIndicator() {
+        circularPagerIndicator?.setupWithViewPager(mFlippingPager, true)
+    }
+
+    /**
+     * Use for setting up of FlipperLayout, instantiating view pager, pager indicator
+     * and binding the adapter with the view pager
+     *
+     * @param context for Inflater
+     */
+    private fun setLayout(context: Context) {
+        flipperLayout = LayoutInflater.from(context).inflate(
+                R.layout.flipper_layout, this, true)
+        mFlippingPager = flipperLayout!!.findViewById(R.id.vp_flipper_layout)
+        circularPagerIndicator = flipperLayout!!.findViewById(R.id.tabLayout)
+
+        mFlippingPagerAdapter = FlipperAdapter()
+        mFlippingPager.adapter = mFlippingPagerAdapter
+        circularPagerIndicator!!.setupWithViewPager(mFlippingPager, true)
+
+        // Handler for onPageChangeListener
+        circularFlipperHandler = CircularFlipperHandler(mFlippingPager)
+        circularFlipperHandler.setCurrentPageListener(this)
+        mFlippingPager.addOnPageChangeListener(circularFlipperHandler)
+
+        //Starting auto cycle at the time of setting up of layout
+        startAutoCycle()
+    }
+
+    /**
+     * Add Flipper View to the pager adapter
+     *
+     * @param flipperView is sent as the view to be added to the adapter
+     */
+    fun addFlipperView(flipperView: FlipperView) {
+        flipperView.setViewHeight(flipperLayout!!.layoutParams.height)
+        mFlippingPagerAdapter.addFlipperView(flipperView)
+        //        pagerIndicator.setViewPager(mFlippingPager);
+    }
+
+    /**
+     * Method to start Auto Cycle using Handler, Runnable and Timer
+     */
+    private fun startAutoCycle() {
+        if (flippingTimer != null) {
+            flippingTimer!!.cancel()
+            flippingTimer!!.purge()
+        }
+        flippingTimer = Timer()
+        flippingTimer!!.schedule(object : TimerTask() {
+            override fun run() {
+                flipperHandler.post(update)
+            }
+        }, DELAY_MS, (this.scrollTimeInSec * 1000).toLong())
+    }
+
+    override fun onCurrentPageChanged(currentPosition: Int) {
+        this.currentPage = currentPosition
+    }
+
+
+    /**
+     * @param reverseDrawingOrder true if the supplied PageTransformer requires page views
+     * to be drawn from last to first instead of first to last.
+     * @param transformer         PageTransformer that will modify each page's animation properties
+     * @link {https://developer.android.com/training/animation/screen-slide.html#kotlin}
+     * Method to add Page transformer into the Flipper layout
+     */
+    fun addPageTransformer(reverseDrawingOrder: Boolean,
+                           transformer: ViewPager.PageTransformer?) {
+        mFlippingPager.setPageTransformer(reverseDrawingOrder, transformer)
+    }
+
+    companion object {
+
+        /**
+         * Delay for Timer Task
+         */
+        private const val DELAY_MS: Long = 500
+    }
+}
