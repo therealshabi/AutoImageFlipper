4202f7afdef74e8034d9cad11771efa43cf6a6af
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-09-15 22:12:55 +0530

    Add two custom pager transformer taken from developer.android.com

---
+++ imageslider/src/main/java/technolifestyle/com/imageslider/pagetransformers/ZoomOutPageTransformer.kt
@@ -0,0 +1,49 @@
+package technolifestyle.com.imageslider.pagetransformers
+
+import android.view.View
+import androidx.viewpager.widget.ViewPager
+import kotlin.math.abs
+
+private const val MIN_SCALE = 0.85f
+private const val MIN_ALPHA = 0.5f
+
+/**
+ * @link {https://developer.android.com/training/animation/screen-slide.html#kotlin}
+ */
+class ZoomOutPageTransformer : ViewPager.PageTransformer {
+    override fun transformPage(view: View, position: Float) {
+        view.apply {
+            val pageWidth = width
+            val pageHeight = height
+            when {
+                position < -1 -> { // [-Infinity,-1)
+                    // This page is way off-screen to the left.
+                    alpha = 0f
+                }
+                position <= 1 -> { // [-1,1]
+                    // Modify the default slide transition to shrink the page as well
+                    val scaleFactor = Math.max(MIN_SCALE, 1 - abs(position))
+                    val verticalMargin = pageHeight * (1 - scaleFactor) / 2
+                    val horizontalMargin = pageWidth * (1 - scaleFactor) / 2
+                    translationX = if (position < 0) {
+                        horizontalMargin - verticalMargin / 2
+                    } else {
+                        horizontalMargin + verticalMargin / 2
+                    }
+
+                    // Scale the page down (between MIN_SCALE and 1)
+                    scaleX = scaleFactor
+                    scaleY = scaleFactor
+
+                    // Fade the page relative to its size.
+                    alpha = (MIN_ALPHA +
+                            (((scaleFactor - MIN_SCALE) / (1 - MIN_SCALE)) * (1 - MIN_ALPHA)))
+                }
+                else -> { // (1,+Infinity]
+                    // This page is way off-screen to the right.
+                    alpha = 0f
+                }
+            }
+        }
+    }
+}
