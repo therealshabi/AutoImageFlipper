369ad29ecc9bc2f906c66f3a942122f8a25d2da3
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-10-31 15:21:53 +0530

    Add recycler view in HomeActivity

--- app/src/main/java/technolifestyle/com/autoimageslider/home/HomeActivity.kt
+++ app/src/main/java/technolifestyle/com/autoimageslider/home/HomeActivity.kt
@@ -2,6 +2,8 @@ package technolifestyle.com.autoimageslider.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_home.*
 import technolifestyle.com.autoimageslider.R
 
 class HomeActivity : AppCompatActivity() {
@@ -9,5 +11,32 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val arrayList = ArrayList<HomeContents>()
+
+        arrayList.add(HomeContents(HomeType.Image("https://blog.eap.ucop.edu/wp-content/uploads/2016/01/Julie-Huang-27.jpg", "Test Image 1")))
+        arrayList.add(HomeContents(HomeType.ImageFlipper(ArrayList(listOf(
+                HomeType.Image("https://blog.eap.ucop.edu/wp-content/uploads/2016/01/Julie-Huang-27.jpg", "Flipper Test Image 1"),
+                HomeType.Image("https://source.unsplash.com/random", "Flipper Test Image 2"),
+                HomeType.Image("https://i.pinimg.com/originals/18/40/72/184072abb72399c23ab635faaa0a94ad.jpg", "Flipper Test Image 3")
+        )))))
+        arrayList.add(HomeContents(HomeType.Image("https://source.unsplash.com/random", "Test Image 2")))
+        arrayList.add(HomeContents(HomeType.Image("https://blog.eap.ucop.edu/wp-content/uploads/2016/01/Julie-Huang-27.jpg", "Test Image 3")))
+        arrayList.add(HomeContents(HomeType.Image("https://i.pinimg.com/originals/18/40/72/184072abb72399c23ab635faaa0a94ad.jpg", "Test Image 4")))
+        arrayList.add(HomeContents(HomeType.ImageFlipper(ArrayList(listOf(
+                HomeType.Image("https://source.unsplash.com/random", "Flipper Test Image 4"),
+                HomeType.Image("https://i.pinimg.com/originals/18/40/72/184072abb72399c23ab635faaa0a94ad.jpg", "Flipper Test Image 5"),
+                HomeType.Image("https://blog.eap.ucop.edu/wp-content/uploads/2016/01/Julie-Huang-27.jpg", "Flipper Test Image 6")
+        )))))
+
+        homeRecyclerView.layoutManager = LinearLayoutManager(this)
+        homeRecyclerView.adapter = HomeRecyclerAdapter(arrayList)
+    }
+
+    data class HomeContents(val type: HomeType)
+
+    sealed class HomeType {
+        data class Image(val imageUrl: String, val text: String) : HomeType()
+        data class ImageFlipper(val flipperList: ArrayList<Image>) : HomeType()
     }
 }
---
+++ app/src/main/java/technolifestyle/com/autoimageslider/home/HomeRecyclerAdapter.kt
@@ -0,0 +1,68 @@
+package technolifestyle.com.autoimageslider.home
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.image_item.view.*
+import technolifestyle.com.autoimageslider.R
+import technolifestyle.com.imageslider.FlipperView
+
+class HomeRecyclerAdapter(private val homeContentList: ArrayList<HomeActivity.HomeContents>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return (when (viewType) {
+            0 -> HomeImageViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.image_item, parent, false))
+            1 -> HomeImageFlipperViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.activity_main, parent, false))
+            else -> HomeImageViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.image_item, parent, false))
+        })
+    }
+
+    override fun getItemCount(): Int {
+        return homeContentList.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (homeContentList[position].type) {
+            is HomeActivity.HomeType.Image -> 0
+            is HomeActivity.HomeType.ImageFlipper -> 1
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder.itemViewType) {
+            0 -> (holder as HomeImageViewHolder).bind(homeContentList[position])
+            1 -> (holder as HomeImageFlipperViewHolder).bind(homeContentList[position])
+        }
+    }
+
+    internal class HomeImageViewHolder(private val homeImageItemView: View) :
+            RecyclerView.ViewHolder(homeImageItemView) {
+        fun bind(homeContents: HomeActivity.HomeContents) {
+            Picasso.get().load(
+                    (homeContents.type as HomeActivity.HomeType.Image).imageUrl)
+                    .into(homeImageItemView.imageview)
+            homeImageItemView.textView.text = homeContents.type.text
+        }
+    }
+
+    internal class HomeImageFlipperViewHolder(private val homeFlipperItemView: View) :
+            RecyclerView.ViewHolder(homeFlipperItemView) {
+        fun bind(homeContents: HomeActivity.HomeContents) {
+            homeFlipperItemView.flipper_layout.setCircleIndicatorHeight(20)
+            (homeContents.type as HomeActivity.HomeType.ImageFlipper).flipperList.forEach {
+                homeFlipperItemView.flipper_layout.addFlipperView(
+                        FlipperView(homeFlipperItemView.context).setDescription(it.text)
+                                .setImageUrl(it.imageUrl) { flipperImageView, imageUrl ->
+                                    Picasso.get()
+                                            .load(imageUrl)
+                                            .into(flipperImageView)
+                                }
+                )
+            }
+        }
+    }
+}
+
+
