dbae85c86c6b2408453e21ca6fd0056af615a383
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-10-31 15:21:31 +0530

    Create new HomeActivity

---
+++ app/src/main/java/technolifestyle/com/autoimageslider/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package technolifestyle.com.autoimageslider.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import technolifestyle.com.autoimageslider.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
